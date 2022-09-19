504ce89ecf0f90492a345a2f5bad07dfc733abec
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-09-19 16:00:28 -0700

    Add support for nesting links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { readdirSync } from 'fs';
 import Link from 'next/link';
 import path from 'path';
+import fs from 'fs';
 
 interface PageLink {
 	href: string;
@@ -15,20 +15,33 @@ const upperCaseWord = (word: string): string => word[0].toUpperCase() + word.sli
 
 export const getStaticProps = async () => {
 	const dir = path.join(process.cwd(), 'pages');
-	const files = readdirSync(dir);
-	const pageLinks: PageLink[] = await Promise.all(
-		files
-			.filter((file) => file.endsWith('.mdx'))
-			.map(async (file) => {
-				const href = file.split('.')[0];
-				const title = href.split('-').map(upperCaseWord).join(' ');
 
-				return {
-					href,
-					title,
-				};
-			})
-	);
+	const getFilesRecursively = (directory: string): string[] => {
+		const files: string[] = [];
+
+		const filesInDirectory = fs.readdirSync(directory);
+		for (const file of filesInDirectory) {
+			const absolute = path.join(directory, file);
+			if (fs.statSync(absolute).isDirectory()) {
+				files.push(...getFilesRecursively(absolute));
+			} else {
+				files.push(absolute.replace(dir, ''));
+			}
+		}
+
+		return files;
+	};
+
+	const pageLinks: PageLink[] = getFilesRecursively(dir)
+		.filter((file) => file.endsWith('.mdx'))
+		.map((file) => {
+			const href = file.split('.')[0].slice(1); //remove extension and starting slash
+			const title = href.split('-').map(upperCaseWord).join(' ');
+			return {
+				href,
+				title,
+			};
+		});
 
 	const props: PageProps = { pageLinks };
 	return { props };
