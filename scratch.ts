111fe6e94a08c4ba689e80f79f0f1e5fcca7c48a
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-09-20 09:45:40 -0700

    OOP Basics typo and Strategy design pattern

--- scratch.ts
+++ scratch.ts
@@ -1,14 +1,37 @@
-export interface NamedCreature {
-	name: string;
-	printName: () => void;
+interface PaymentProvider {
+	pay(): void;
 }
 
-class Puppy implements NamedCreature {
-	name: string = 'Puppy';
-	printName() {
-		console.log(this.name);
+class PayPal implements PaymentProvider {
+	pay(): void {
+		console.log('PAYING WITH PAYPAL');
 	}
 }
 
-const pup = new Puppy();
-pup.printName();
+class Stripe implements PaymentProvider {
+	pay(): void {
+		console.log('PAYING WITH STRIPE');
+	}
+}
+
+export class PaymentProcessor {
+	paymentProvider!: PaymentProvider;
+
+	constructor(paymentProvider: PaymentProvider) {
+		this.paymentProvider = paymentProvider;
+	}
+
+	public setPaymentProvider(newProvider: PaymentProvider) {
+		this.paymentProvider = newProvider;
+	}
+
+	public processPayment() {
+		this.paymentProvider.pay();
+	}
+}
+
+const processor = new PaymentProcessor(new PayPal());
+processor.processPayment();
+
+processor.setPaymentProvider(new Stripe());
+processor.processPayment();
