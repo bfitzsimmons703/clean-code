5dc3b437a5ef30410df3cec92736fd68b685e3d8
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-09-29 10:34:57 -0700

    Code smells

--- scratch.ts
+++ scratch.ts
@@ -1,70 +1,26 @@
-export enum QuestionType {
-	Boolean = 'boolean',
-	MultipleChoice = 'multiplechoice',
-	TextAnswer = 'textanswer',
+interface Reader {
+	read(): string;
 }
 
-abstract class Question {
-	type!: QuestionType;
-	description!: string;
-	abstract print(): void;
-}
-
-function printQuestions(questions: Question[]) {
-	for (const question of questions) {
-		question.print();
+class KeyboardReader implements Reader {
+	read(): string {
+		return 'Hey';
 	}
 }
 
-class BooleanQuestion implements Question {
-	type = QuestionType.Boolean;
-	description: string;
+interface Writer {
+	write(s: string): void;
+}
 
-	constructor(description: string) {
-		this.description = description;
-	}
-
-	print(): void {
-		console.log(this.description);
-		console.log(`1. True`);
-		console.log(`2. False`);
+class ConsoleWriter implements Writer {
+	write(s: string): void {
+		console.log(s);
 	}
 }
 
-class MultipleChoiceQuestion implements Question {
-	type = QuestionType.MultipleChoice;
-	description: string;
-	options: string[];
-
-	constructor(description: string, options: string[]) {
-		this.description = description;
-		this.options = options;
-	}
-
-	print(): void {
-		console.log(this.description);
-		this.options.forEach((option, i) => console.log(`${i + 1}. ${option}`));
-	}
+export function copyChars(reader: Reader, writer: Writer) {
+	const chars = reader.read();
+	writer.write(chars);
 }
 
-class TextAnswerQuestion implements Question {
-	type = QuestionType.TextAnswer;
-	description: string;
-
-	constructor(description: string) {
-		this.description = description;
-	}
-
-	print(): void {
-		console.log(this.description);
-		console.log('Answer: ______________');
-	}
-}
-
-const questions: Question[] = [
-	new BooleanQuestion('Coding is fun.'),
-	new MultipleChoiceQuestion('How many years have you coded?', ['1 year', '2 years', '3 years']),
-	new TextAnswerQuestion('What was your favorite project?'),
-];
-
-printQuestions(questions);
+copyChars(new KeyboardReader(), new ConsoleWriter());
