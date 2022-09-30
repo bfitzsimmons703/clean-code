fd3b7acede1e712c3f6cee47a863e96de94897f9
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-09-30 10:29:56 -0700

    More on testing

--- scratch.ts
+++ scratch.ts
@@ -1,26 +1,70 @@
-interface Reader {
-	read(): string;
+interface EmployeeDatabase {
+	paymentPosted(): boolean;
+	get(employeeId: number): Employee;
+	save(employee: Employee): void;
 }
 
-class KeyboardReader implements Reader {
-	read(): string {
-		return 'Hey';
+interface CheckWriter {
+	writeCheck(amount: number): void;
+	checkWasWritten(): boolean;
+}
+
+class Employee {
+	calculatePayment(): number {
+		//...
+	}
+
+	postPayment(payment: number): void {
+		//...
 	}
 }
 
-interface Writer {
-	write(s: string): void;
-}
+class Payroll {
+	private employeeDB: EmployeeDatabase;
+	private checkWriter: CheckWriter;
 
-class ConsoleWriter implements Writer {
-	write(s: string): void {
-		console.log(s);
+	constructor(employeeDB: EmployeeDatabase, checkWriter: CheckWriter) {
+		this.employeeDB = employeeDB;
+		this.checkWriter = checkWriter;
+	}
+
+	payEmployee() {
+		const employee = this.employeeDB.get(5);
+		const payment = employee.calculatePayment();
+		this.checkWriter.writeCheck(payment);
+		employee.postPayment(payment);
+		this.employeeDB.save(employee);
 	}
 }
 
-export function copyChars(reader: Reader, writer: Writer) {
-	const chars = reader.read();
-	writer.write(chars);
+class MockEmployeeDB implements EmployeeDatabase {
+	paymentPosted(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get(employeeId: number): Employee {
+		throw new Error('Method not implemented.');
+	}
+	save(employee: Employee): void {
+		throw new Error('Method not implemented.');
+	}
 }
 
-copyChars(new KeyboardReader(), new ConsoleWriter());
+class MockCheckWriter implements CheckWriter {
+	writeCheck(amount: number): void {
+		throw new Error('Method not implemented.');
+	}
+	checkWasWritten(): boolean {
+		throw new Error('Method not implemented.');
+	}
+}
+
+test('Test Payroll writes check', () => {
+	const employeeDB = new MockEmployeeDB();
+	const checkWriter = new MockCheckWriter();
+
+	const payroll = new Payroll(employeeDB, checkWriter);
+	payroll.payEmployee();
+
+	expect(checkWriter.checkWasWritten());
+	expect(employeeDB.paymentPosted());
+});
