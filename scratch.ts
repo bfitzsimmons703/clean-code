b75f2921558a46173f888c195e50d6c1a43da03d
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-10-07 10:07:19 -0700

    Command design pattern

--- scratch.ts
+++ scratch.ts
@@ -1,20 +1,68 @@
-export interface Timer {
-	setTimer(): void;
+// Command
+export interface Command {
+	do(): void;
+	undo(): void;
 }
 
-export interface Door {
-	lock(): void;
-	unlock(): void;
+class SwitchLightsCommand implements Command {
+	private light: Light;
+
+	constructor(light: Light) {
+		this.light = light;
+	}
+
+	do(): void {
+		this.light.switchLight();
+	}
+
+	undo(): void {
+		this.light.switchLight();
+	}
 }
 
-class TimedDoor implements Timer, Door {
-	setTimer(): void {
-		throw new Error('Method not implemented.');
+// Receivers
+interface Light {
+	isOn: boolean;
+	switchLight(): void;
+}
+
+class FloorLamp implements Light {
+	isOn: boolean = false;
+	switchLight() {
+		this.isOn = !this.isOn;
+		console.log(`Floor Lamp is ${this.isOn ? 'on' : 'off'}`);
 	}
-	lock(): void {
-		throw new Error('Method not implemented.');
+}
+
+class OverheadLight implements Light {
+	isOn: boolean = false;
+	switchLight() {
+		this.isOn = !this.isOn;
+		console.log(`Overhead Light is ${this.isOn ? 'on' : 'off'}`);
 	}
-	unlock(): void {
-		throw new Error('Method not implemented.');
+}
+
+// Invoker
+abstract class Room {
+	private command!: Command;
+
+	protected setCommand(command: Command) {
+		this.command = command;
 	}
+
+	public executeCommand() {
+		this.command.do();
+	}
+}
+
+class LivingRoom extends Room {
+	constructor() {
+		super();
+		this.setCommand(new SwitchLightsCommand(new FloorLamp()));
+	}
+}
+
+{
+	const livingRoom = new LivingRoom();
+	livingRoom.executeCommand();
 }
