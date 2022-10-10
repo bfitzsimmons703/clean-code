16fdae30110d018c0256fc29fb2931c8e96661be
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-10-10 10:50:11 -0700

    Mediator example

--- scratch.ts
+++ scratch.ts
@@ -1,13 +1,52 @@
-interface Connection {
-	insert(s: string): void;
+class User {
+	private mediator: ChatMediator;
+	private name: string;
+
+	constructor(mediator: ChatMediator, name: string) {
+		this.mediator = mediator;
+		this.name = name;
+		this.mediator.registerUser(this);
+	}
+
+	send(message: string) {
+		this.mediator.broadcast(message, this);
+	}
+
+	receive(message: string) {
+		console.log(`${this.name} received message '${message}'`);
+	}
 }
 
-export class DB {
-	connection!: Connection;
+export interface ChatMediator {
+	users: Set<User>;
+	broadcast(message: string, from: User): void;
+	registerUser(user: User): void;
+}
 
-	init() {}
+class ChatMediatorImpl implements ChatMediator {
+	users: Set<User> = new Set();
 
-	insertProducts() {
-		this.connection.insert('...');
+	broadcast(message: string, from: User): void {
+		for (const user of this.users) {
+			if (user !== from) {
+				user.receive(message);
+			}
+		}
 	}
+
+	registerUser(user: User): void {
+		this.users.add(user);
+	}
+}
+
+{
+	const mediator = new ChatMediatorImpl();
+
+	const brian = new User(mediator, 'Brian');
+	const jesse = new User(mediator, 'Jesse');
+	const katie = new User(mediator, 'Katie');
+
+	brian.send('Hi!');
+	jesse.send('Hey back!');
+	katie.send('Allo!');
 }
