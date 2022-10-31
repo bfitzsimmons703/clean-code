09649a0b4a420116f07be3d8ce70adad67f2367c
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-10-30 20:29:51 -0700

    More on general smells and heuristics

--- scratch.ts
+++ scratch.ts
@@ -1,52 +1,15 @@
-class User {
-	private mediator: ChatMediator;
-	private name: string;
-
-	constructor(mediator: ChatMediator, name: string) {
-		this.mediator = mediator;
-		this.name = name;
-		this.mediator.registerUser(this);
-	}
-
-	send(message: string) {
-		this.mediator.broadcast(message, this);
-	}
-
-	receive(message: string) {
-		console.log(`${this.name} received message '${message}'`);
-	}
+export enum Day {
+	Monday,
+	Tuesday,
 }
 
-export interface ChatMediator {
-	users: Set<User>;
-	broadcast(message: string, from: User): void;
-	registerUser(user: User): void;
-}
-
-class ChatMediatorImpl implements ChatMediator {
-	users: Set<User> = new Set();
-
-	broadcast(message: string, from: User): void {
-		for (const user of this.users) {
-			if (user !== from) {
-				user.receive(message);
-			}
-		}
+const stringToDay = (dayName: string): Day => {
+	switch (dayName.toLowerCase()) {
+		case 'monday':
+			return Day.Monday;
+		case 'tuesday':
+			return Day.Tuesday;
+		default:
+			throw new Error('Unknown string day name');
 	}
-
-	registerUser(user: User): void {
-		this.users.add(user);
-	}
-}
-
-{
-	const mediator = new ChatMediatorImpl();
-
-	const brian = new User(mediator, 'Brian');
-	const jesse = new User(mediator, 'Jesse');
-	const katie = new User(mediator, 'Katie');
-
-	brian.send('Hi!');
-	jesse.send('Hey back!');
-	katie.send('Allo!');
-}
+};
